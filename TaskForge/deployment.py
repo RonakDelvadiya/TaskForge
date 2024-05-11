f072ce5a2a43a755cae24ad5597ddf097835f6ff
Ronak <dronak@trellissoft.ai>
2024-05-11 17:12:25 +0530

    Change .gitignore and added deployment related files.

---
+++ TaskForge/deployment.py
@@ -0,0 +1,29 @@
+import os 
+from .settings import *
+from .settings import BASE_DIR
+
+
+SECRET_KEY = os.environ['SECRET']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
+
+# WhiteNoise configuration
+MIDDLEWARE = [
+    "django.middleware.cache.UpdateCacheMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "TaskForge.middlewares.LoggingMiddleware",
+    "django.middleware.cache.FetchFromCacheMiddleware"
+]
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
