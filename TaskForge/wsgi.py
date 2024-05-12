0e4a0f6dbe3c081f183f0ddbe30a9eea7f33d096
Ronak <dronak@trellissoft.ai>
2024-05-12 12:34:26 +0530

    Change in manage.py file

--- TaskForge/wsgi.py
+++ TaskForge/wsgi.py
@@ -2,7 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 settings_module = 'TaskForge.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'TaskForge.settings'
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
